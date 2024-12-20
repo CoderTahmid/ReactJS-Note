82ae452ba972e4f49e9557e5f607540671738163
CoderTahmid <tahmid.ibne@gmail.com>
2024-12-20 21:48:13 +0600

    first commit

---
+++ my-first-react/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>King</h1>
+    </>
+  )
+};
+
+// function Users() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(res => res.json())
+//       .then(data => console.log(data));
+//   }, []);
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// };
+
+export default App
+
