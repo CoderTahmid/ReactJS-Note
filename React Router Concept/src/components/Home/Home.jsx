e25828f499ad20e5b7bca36dc2982099766aa8d8
CoderTahmid <tahmid.ibne@gmail.com>
2025-03-29 13:33:00 +0600

    usage of useNavigation to show somehing different in the UI while data loading

--- React Router Concept/src/components/Home/Home.jsx
+++ React Router Concept/src/components/Home/Home.jsx
@@ -1,12 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Home = () => {
+
+    const navigation = useNavigation();
+
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ? 
+                <p>Loading...</p> : 
+                <Outlet></Outlet>
+                /*
+                ekhane ja kora hocche muloto 
+                useNavigation use kore 
+                loading screen ke handle kora
+                jodi status loading hoy tahole
+                "Loading..." likha ashbe
+                ar statsu loading na hoile <outlet/> component ta show korbe arki
+                */
+            }
             <Footer></Footer>
         </div>
     );
