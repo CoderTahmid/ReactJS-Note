0c6bbe5ae1051f3b952bc21bc2801c8df55baf58
CoderTahmid <tahmid.ibne@gmail.com>
2025-03-29 12:47:22 +0600

    show error message in UI

--- React Router Concept/src/components/ErrorPage/ErrorPage.jsx
+++ React Router Concept/src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,22 @@
+import { Link, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+
+    const error = useRouteError();
+    console.log(error); // eta console korbe je ki error ashche arki
+
     return (
         <div>
             <h2>Opps!! Error occured</h2>
+            <p>{error.statusText || error.message}</p>
+            {
+                error.status === 404 &&
+                <div>
+                    <h3>Page not found, please</h3>
+                    <p>Go back where you from</p>
+                    <Link to="/"><button>Home</button></Link>
+                </div>
+            }
         </div>
     );
 };
