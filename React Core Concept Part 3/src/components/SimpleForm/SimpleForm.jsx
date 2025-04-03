e54681c9f671d9cf091c305f964285260bb51914
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-03 06:50:37 +0600

    using form in ReactJS, shown how to get field value from a form

---
+++ React Core Concept Part 3/src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,23 @@
+const SimpleForm = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("Form submitted");
+        console.log(e.target.name.value); // output : field e jei value dewa hbeh
+        console.log(e.target.email.value); // outpu : field e jei value dewa hbeh
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="submit" value="submit"/>
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
