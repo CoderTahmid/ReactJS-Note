916dc251eaafdae98b140418f6cc23b8e8186abd
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-04 11:57:50 +0600

    using reusable component

--- React Core Concept Part 3/src/App.jsx
+++ React Core Concept Part 3/src/App.jsx
@@ -1,17 +1,38 @@
 import './App.css'
 import HookForm from './components/HookForm/HookForm'
 import RefForm from './components/RefForm/RefForm'
+import ReuseableForm from './components/ReuseableForm/ReuseableForm'
 import SimpleForm from './components/SimpleForm/SimpleForm'
 import StatefulForm from './components/StatefulForm/StatefulForm'
 
 function App() {
+
+  const handleSignUpSubmit = data => {
+    // e.preventDefault();
+    console.log("Sign up data", data);
+  };
+
+  const handleUpdateProfile = data => {
+    // e.preventDefault();
+    console.log("Update form data", data);
+  };
+
   return (
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReuseableForm
+        formTitle={"Sign Up"}
+        handleSubmit={handleSignUpSubmit}>
+      </ReuseableForm>
+      <ReuseableForm
+        formTitle={"Profile Update"}
+        submitBtnText={"Update"}
+        handleSubmit={handleUpdateProfile}>
+      </ReuseableForm>
     </>
   )
 }
