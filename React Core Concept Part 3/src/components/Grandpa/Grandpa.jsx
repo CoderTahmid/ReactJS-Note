d825dab727024dcef19a7734b37f2e6feaa22a36
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-05 15:41:34 +0600

    end of React Core Concept Part 3

--- React Core Concept Part 3/src/components/Grandpa/Grandpa.jsx
+++ React Core Concept Part 3/src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import "./Grandpa.css";
 
 export const AssetContext = createContext("Gold");
-    /*
-    eta ke must export korte hbeh
-    naile onno jayga theke import kora jabe nah
-    */
+export const MoneyContext = createContext(1000);
+/*
+eta ke must export korte hbeh
+naile onno jayga theke import kora jabe nah
+*/
 const Grandpa = () => {
+
+    const [money, setMoney] = useState(1000);
+
     return (
         <div className="grandpa">
             <h2>Grandpa</h2>
-            <AssetContext.Provider value="gold">
+            <p>Net money : {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
                     {/*
                     according to Jhankar bhai ekhane provider boshano mane hocche
                     tower boshano
@@ -21,12 +27,13 @@ const Grandpa = () => {
                     tower boshano hoise ekhn shei tower theke signal recieve korbe bibhinno phone
                     I mean bibhinno childs
                     */}
-                <section className="flex">
-                    <Dad></Dad>
-                    <Uncle></Uncle>
-                    <Aunty></Aunty>
-                </section>
-            </AssetContext.Provider>
+                    <section className="flex">
+                        <Dad></Dad>
+                        <Uncle></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
